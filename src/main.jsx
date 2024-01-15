4a218c1272dc96cd175422f587934b22a568be6f
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2024-01-16 01:59:28 +0600

    React Strictmode implemented

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,13 @@ import 'react-date-range/dist/styles.css'
 import 'react-date-range/dist/theme/default.css'
 import { HelmetProvider } from 'react-helmet-async'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React from 'react'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HelmetProvider>
+ <React.StrictMode>
+   <HelmetProvider>
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
         <Toaster />
@@ -20,4 +22,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </QueryClientProvider>
     </AuthProvider>
   </HelmetProvider>
+ </React.StrictMode>
 )
