5fd4e35b762e2a672aaabe3c9eb4babe56b00971
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2024-01-16 19:49:40 +0600

    showed user data using TenStack Query

--- src/api/auth.js
+++ src/api/auth.js
@@ -29,3 +29,9 @@ export const getRole = async (email) => {
   const { data } = await axiosSecure(`/user/${email}`);
   return data.role;
 };
+
+// Get All Users
+export const getAllUsers = async () => {
+  const { data } = await axiosSecure('/users');
+  return data;
+};
