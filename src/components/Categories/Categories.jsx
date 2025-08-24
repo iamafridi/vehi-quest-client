1259d192ef8d183031ba379d3c2428185ef81023
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2025-08-24 20:34:02 +0600

    Updated Home Page

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -4,20 +4,32 @@ import { categories } from "./CategoriesData.js";
 import CategoryBox from "./CategoryBox";
 
 const Categories = () => {
-
-    const [params, setParams] = useSearchParams();
-    const category = params.get('category')
-    console.log(category);
+    const [params] = useSearchParams();
+    const category = params.get("category");
 
     return (
         <Container>
-            <div className="pt-4 flex items-center justify-between overflow-x-auto">
-                {categories.map(item => <CategoryBox
-                    key={item.label}
-                    label={item.label}
-                    icon={item.icon}
-                    selected={category === item.label}
-                ></CategoryBox>)}
+            <div
+                className=" border-2 shadow-slate-400 shadow-lg rounded-full mt-2
+          pt-4 
+          flex 
+          gap-4
+          overflow-x-auto 
+          sm:grid 
+          sm:grid-cols-2 
+          md:grid-cols-4 
+          lg:grid-cols-8
+          xl:grid-cols-8
+        "
+            >
+                {categories.map((item) => (
+                    <CategoryBox
+                        key={item.label}
+                        label={item.label}
+                        icon={item.icon}
+                        selected={category === item.label}
+                    />
+                ))}
             </div>
         </Container>
     );
